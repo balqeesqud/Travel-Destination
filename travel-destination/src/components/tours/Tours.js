c3675ceec92c82a69c50019d24a2aabd46316585
balqees <balqeesalqudah97@gmail.com>
2023-07-10 12:16:07 +0300

    render the Tours in Home component, mapping inside Tours, added a key

--- travel-destination/src/components/tours/Tours.js
+++ travel-destination/src/components/tours/Tours.js
@@ -1,11 +1,31 @@
 import './Tours.css';
+import db from '../../data/db.json';
+
+//name and image props as arguments using destructuring
+// it can be written without destructuring (props) is an object
+//const TourItem = (props) => {
+// const { name, image } = props;
+
 function Tours(props) {
+  const TourElement = ({ name, image }) => (
+    <div>
+      <h2>{name}</h2>
+      <img src={image} alt={name} />
+    </div>
+  );
+
   return (
-    <div class="home-content">
+    <div className="home-content">
       <h2>{props.name}</h2>
       <img src={props.image} alt={props.name} />
+
+      {db.map((obj, i) => (
+        <TourElement key={i} name={obj.name} image={obj.image} />
+      ))}
     </div>
   );
 }
 
 export default Tours;
+
+//+++++++
